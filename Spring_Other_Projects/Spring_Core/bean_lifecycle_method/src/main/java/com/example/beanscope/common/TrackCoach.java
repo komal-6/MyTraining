d4395f8a24bb3a174dbb89b6852ed3614ab5a0bb
Komal Panchal <93608292+komal-6@users.noreply.github.com>
2023-08-31 17:59:17 +0530

    Main File

---
+++ Spring_Other_Projects/Spring_Core/bean_lifecycle_method/src/main/java/com/example/beanscope/common/TrackCoach.java
@@ -0,0 +1,16 @@
+package com.example.beanscope.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackCoach implements Coach{
+
+
+    public TrackCoach(){
+        System.out.println("In constructor: "+getClass().getSimpleName());
+    }
+    @Override
+    public String getDailyWorkout() {
+        return "TrackCoach getDailyWorkout method";
+    }
+}
