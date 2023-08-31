0485873d3af1d9ed1733b8d2a798d81fcb39022e
Komal Panchal <93608292+komal-6@users.noreply.github.com>
2023-08-31 12:56:38 +0530

    Main File

---
+++ ApiGateway_Example/EurekDemo/src/test/java/com/example/EurekDemo/EurekDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EurekDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
