c6336753cd1d5c2322366d99458147941941468e
Komal Panchal <93608292+komal-6@users.noreply.github.com>
2023-08-31 14:53:20 +0530

    Main File

---
+++ Integration/microserviceCloudExample/src/main/java/com/apigateway/microserviceCloudExample/MicroserviceCloudExampleApplication.java
@@ -0,0 +1,17 @@
+package com.apigateway.microserviceCloudExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaServer
+public class MicroserviceCloudExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCloudExampleApplication.class, args);
+	}
+
+}
---
+++ Integration/microserviceCloudExample/src/test/java/com/apigateway/microserviceCloudExample/MicroserviceCloudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apigateway.microserviceCloudExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCloudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
