7e4b8bcee979e23c266541d1cd95e1c18f269906
Komal Panchal <93608292+komal-6@users.noreply.github.com>
2024-04-29 15:22:22 +0530

    Add src folder

---
+++ Spring/EurekaServerDemo/Eurekademo/src/test/java/com/example/Eurekdemo/EurekdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Eurekdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
