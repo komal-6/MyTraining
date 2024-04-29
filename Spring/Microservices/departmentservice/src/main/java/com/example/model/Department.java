358c58d459ab300bf63ffe006d5c2edc469d540e
Komal Panchal <93608292+komal-6@users.noreply.github.com>
2024-04-29 11:55:14 +0530

    Add src folder

---
+++ Spring/Microservices/departmentservice/src/main/java/com/example/model/Department.java
@@ -0,0 +1,75 @@
+package com.example.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.annotations.ManyToAny;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
+@Entity
+@Table(name="dept_data")
+public class Department {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int dept_id;
+	private String dept_name;
+
+	@Transient
+	private List<User> users = new ArrayList<User>();
+
+	public Department() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Department(int dept_id, String dept_name, List<User> users) {
+		super();
+		this.dept_id = dept_id;
+		this.dept_name = dept_name;
+		this.users = users;
+	}
+
+	public int getDept_id() {
+		return dept_id;
+	}
+
+	public void setDept_id(int dept_id) {
+		this.dept_id = dept_id;
+	}
+
+	public String getDept_name() {
+		return dept_name;
+	}
+
+	public void setDept_name(String dept_name) {
+		this.dept_name = dept_name;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [dept_id=" + dept_id + ", dept_name=" + dept_name + ", users=" + users + ", getDept_id()="
+				+ getDept_id() + ", getDept_name()=" + getDept_name() + ", getUsers()=" + getUsers() + ", getClass()="
+				+ getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+	}
+}
