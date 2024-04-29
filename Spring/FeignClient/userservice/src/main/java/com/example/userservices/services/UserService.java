ec0b262bb50be52753974b6275ecc42797ee99c7
Komal Panchal <93608292+komal-6@users.noreply.github.com>
2024-04-29 12:11:07 +0530

    Delete Spring/FeignClient/userservice directory

--- Spring/FeignClient/userservice/src/main/java/com/example/userservices/services/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.userservices.services;
-
-import com.example.userservice.model.User;
-
-public interface UserService {
-	User getUserById(int dept_id);
-	User saveUser(User user);
-}
